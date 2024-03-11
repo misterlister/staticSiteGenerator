7a78d608bd3fc7b877b8d0791b04d3b751240f23
Hayden Lister <haydenglister@gmail.com>
2024-03-10 22:10:34 -0700

    Created TextNode class, created main run script

---
+++ src/textnode.py
@@ -0,0 +1,17 @@
+class TextNode:
+    def __init__(self, text, text_type, url) -> None:
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+    
+    def __eq__(self, node):
+        if self.text != node.text:
+            return False
+        if self.text_type != node.text_type:
+            return False
+        if self.url != node.url:
+            return False
+        return True
+    
+    def __repr__(self):
+        return (f"TextNode({self.text}, {self.text_type}, {self.url})")
