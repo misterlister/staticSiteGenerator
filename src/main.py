fab95c105beeb15937f84ac737a69ac1e6093380
Hayden Lister <haydenglister@gmail.com>
2024-03-30 13:12:07 -0700

    Added page generation to main

--- src/main.py
+++ src/main.py
@@ -1,12 +1,15 @@
 from textnode import TextNode
-from filemanip import delete_directory, copy_dir_contents
+from filemanip import delete_directory, copy_dir_contents, generate_page
 
 static_path = "./static"
 public_path = "./public"
+content_path = "./content"
+template_path = "template.html"
 
 def main():
     delete_directory(public_path)
     copy_dir_contents(static_path, public_path)
+    generate_page(content_path+"/index.md", template_path, public_path+"/index.html")
 
 if __name__ == '__main__':
     main()
